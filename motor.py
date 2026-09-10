0351b8732185c3d1ccc0c4f599fe9abf621126be
Paulo Vallejo <transacciones@nacionalissimo.com>
2026-09-10 09:47:24 -0600

    captura cargo Facebook 9-sep 5AT9E5N9P4 del comprobante PDF

--- motor.py
+++ motor.py
@@ -586,6 +586,7 @@ def procesar_facebook(body):
 CARGOS_MANUALES = [
     ("9YD886J9P4", 5, 9, 2026, 15000.00),
     ("Y5K8Z4N9P4", 6, 9, 2026, 15000.00),
+    ("5AT9E5N9P4", 9, 9, 2026, 15000.00),   # del comprobante PDF FBADS-544-106624295
 ]
 
 def sembrar_cargos_manuales():
